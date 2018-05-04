1ea4ccfb2a99bbe150ca4ea16b38cd38b799bd18
Martin Buessemeyer <Martin.Buessemeyer@hpi.de>
2018-05-04 20:27:31 +0200

    added player hp, player elemental switch

--- Player.gd
+++ Player.gd
@@ -4,11 +4,19 @@ extends KinematicBody
 # var a = 2
 # var b = "textvar"
 var speed = 100
+const elemental_enum = ["water", "fire"]
+var current_element
+var hp
+var elemental_souls_counter = [0, 0]
+const switch_costs_souls = 5 #change for editing the soul costs when switching
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_process(true)
+	hp = 500 #initial HP from the player, can be changed later
+	elemental_souls_counter[0] = 0
+	elemental_souls_counter[1] = 1
 
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -16,4 +24,15 @@ func _process(delta):
 	var input = Vector3(0,0,0)
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	input.z = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	move_and_slide(input.normalized()*speed*delta, Vector3(0,1,0))
+	move_and_slide(input.normalized() * speed * delta, Vector3(0,1,0))
+	
+	#handles the elemental-group-changig !INCOMPELE!
+	if Input.is_action_pressed("ui_accept"):
+		#check if the player has enough souls
+		if current_element == elemental_enum[0] and elemental_souls_counter[1] >= switch_costs_souls:
+			current_element = elemental_enum[1]
+		elif elemental_souls_counter[0] >= switch_costs_souls:
+			current_element = elemental_enum[0]
+		#else:
+			#TODO add message to the player that indicates that he has not enough souls to switch
+			
