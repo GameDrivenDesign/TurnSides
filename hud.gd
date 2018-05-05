bb9bebae61b777e4475532539e0cbe1bbd0fa4dd
Martin Buessemeyer <Martin.Buessemeyer@hpi.de>
2018-05-05 11:44:56 +0200

    resolved merge conflict

--- hud.gd
+++ hud.gd
@@ -16,8 +16,7 @@ func set_elemental_counts(fire, water, hp):
 	$elemental_stats/hp.text = str(hp)
 
 func update_for_player(player):
-<<<<<<< HEAD
-	set_elemental_counts(player.elemental_souls_counter[1], player.elemental_souls_counter[0])
+	set_elemental_counts(player.elemental_souls_counter[1], player.elemental_souls_counter[0], player.hp)
 	
 func showGameOverScreen(score):
 	$GameOverScreen.show()
@@ -25,7 +24,3 @@ func showGameOverScreen(score):
 	var y = $hud.get_viewport().size.y
 	set_rect(Vector2(x * 0.05, y * 0.05))
 	rect_size(Vector2(x * 0.9, y * 0.9))
-	
-=======
-	set_elemental_counts(player.elemental_souls_counter[1], player.elemental_souls_counter[0], player.hp)
->>>>>>> 29331011f594cc6f1abf85689ca41f6f37250376
